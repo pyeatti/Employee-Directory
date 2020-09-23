12a23cec6a88c0fb2f036c1fc1a8f6f2dc57aa71
Ian Pyeatt <pyeatt08@gmail.com>
2020-09-23 08:34:01 -0500

    added sort feature on name header

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -18,6 +18,24 @@ function Table() {
       });
     });
   }, []);
+  const onSortByName = (event) => {
+    setEmployeeState({
+      ...employeeState,
+      filteredEmployees: employeeState.filteredEmployees.sort(function (a, b) {
+        var nameA = a.name.first.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.first.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+
+        // names must be equal
+        return 0;
+      }),
+    });
+  };
 
   const searchForEmployee = (e) => {
     let searchValue = e.target.value;
@@ -41,7 +59,13 @@ function Table() {
         <thead>
           <tr>
             <th>Image</th>
-            <th>Name</th>
+            <th
+              onClick={(event) => {
+                onSortByName(event);
+              }}
+            >
+              Name
+            </th>
             <th>Email</th>
             <th>Phone</th>
             <th>DOB</th>
